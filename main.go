c5bcc3cf6d89f8a9459cc3b92f2dccdf608a459a
Jon Calhoun <joncalhoun@gmail.com>
2015-08-24 09:00:48 -0400

    v1 of socket server

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/schema"
+	"github.com/joncalhoun/webjack"
+)
+
+var server *webjack.Server
+
+type Message struct {
+	Message string `schema:"message"`
+	Channel string `schema:"channel"`
+}
+
+func main() {
+	server = webjack.NewServer()
+	http.Handle("/ws", server.GetHandler())
+	http.HandleFunc("/msg", messageHandler)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	key := r.Header.Get("Authorization")
+	if key == "fWb7CIPUyTx3jZlE5VX9npgmMuQo" {
+		err := r.ParseForm()
+
+		if err != nil {
+			// Handle error
+		}
+
+		decoder := schema.NewDecoder()
+		msg := new(Message)
+		fmt.Println(msg)
+		err = decoder.Decode(msg, r.PostForm)
+		server.SendChannel(msg.Message, msg.Channel)
+	}
+}
